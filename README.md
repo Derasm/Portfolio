0df5d7b1f0189247a83737b17036cf8ec2839fc9
Dennis Rasmussen <RasmussenDennisB@gmail.com>
2025-01-09 20:15:14 +0100

    updated repo and introduction

--- README.md
+++ README.md
@@ -5,10 +5,38 @@ Has a secondary part with Devops showing behind the scenes look at things.
 ## Current tasks:
 - [x] Create a basic website
 - [ ] Add a list of projects
-- [ ] Add a list of Devops projects
-- [ ] Add Gitlab Pages
+- [ ] Add a list of Devops projects (currently: Azure monitoring, prometheos on cluster, web api on cluster)
+- [ ] Add Github Pages
 - [ ] Add Build step in .gitlab-ci.yml
 - [ ] Add CI/CD
 - [ ] Add AWS integration (just to show i can use that one too.)
 - [ ] Add monitoring of page on DevOps part
 - [ ] Add Testing
+
+## DevOps part:
+- [ ] Add a list of Devops projects
+- [ ] Add wep api for consumption of data
+- [x] Add a database for the data
+- [ ] Figure out what kind of data that should be (problaby articles, would be easy.)
+- [ ] Add Markdown viewer to articles page, that consumes and shows the articles.
+- [ ] Add a way to add articles to the database
+- [ ] Add a way to edit articles in the database
+- [ ] Add a way to delete articles in the database
+
+## Database stuff:
+- [x] Add a database for the data
+- [ ] Figure out a schema. Likely: Title, date, tags, content (array of strings), images (array of VarBinary(max))
+- [ ] have a table for error logs. Not sure what kinda error logs yet, but could be neat.
+
+Create script:
+```sql
+CREATE TABLE [dbo].[Articles](
+  [Id] [int] IDENTITY(1,1) NOT NULL,
+  [Title] [nvarchar](50) NOT NULL,
+  [Date] [datetime] NOT NULL,
+  [Tags] [nvarchar](50) NOT NULL,
+  [Content] [nvarchar](max) NOT NULL,
+  [Images] [varbinary](max) NOT NULL,
+ CONSTRAINT [PK_Articles] PRIMARY KEY CLUSTERED 
+)
+```
--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,27 +1,83 @@
 <template>
     <div
-        class="tw-w-full tw-bg-violet-800 tw-flex tw-justify-center tw-text-center tw-text-white tw-align-middle tw-pb-64 tw-pt-8"
+        class="tw-w-full tw-bg-violet-800 tw-flex tw-flex-col md:tw-flex-row tw-justify-center tw-text-center tw-text-white tw-align-middle tw-pb-64 tw-pt-8"
     >
-        <div class="tw-w-1/2 tw-mx-auto tw-pt-24">
-            <span class="tw-text-2xl tw-font-bold">Hi, I'm Dennis.</span>
+        <!-- Text Section -->
+        <div class="tw-w-1/2  tw-pt-24 tw-p-4 tw-mx-24">
+            <span class="tw-text-2xl tw-font-bold tw-text-white">Hi, I'm Dennis.</span>
+            <div class="tw-text-white">
             <p class="tw-text-lg">
                 <br />
-                I got started writing software back in 2017 as a backend, then
-                slowly moved around the stack until i fell in love with DevOps
-                and Cloud development.
+                I got started writing software back around 2014/15 as a backend, then
+                slowly moved around the stack until i fell in love with Cloud, Backend and DevOps.
             </p>
             <p class="tw-text-lg">
                 I've built products for the maritime sector, robotics for
-                hospitals, pipelines for complex products and mentored teams
-                across domains.
+                hospitals, CI pipelines for complex setups and mentored new employees across sectors. <br>
+                I am currently working on Fintech projects for the Hotel sector.
             </p>
+            <q-separator dark />
             <p class="tw-text-lg">
-                I do both Freelance and Fulltime employment, and love working
-                remotely. Technology empowers so many to do so much, and I love
-                being part of the journey.
+                Projects I have headed / been main driving force on:
             </p>
+            </div>
+
+                <ol class="tw-text-lg tw-text-left tw-list-disc tw-w-3/4 tw-mx-auto ">
+                    <li>VueJS based CMS for a maritime company, with massive focus on performance / size of application</li>
+                        <div class="tw-ml-12 tw-text-gray-300 tw-text-sm">
+                            <ol  style="list-style-type: lower-alpha; padding-bottom: 0;">
+                                <li>Reduced load time from 7s to 2s on startup</li>
+                                <li>Reduced initial bundle size from 2.3mb to 840kb using chunking and lazy-load, as well as pruning</li>
+                                <li>implemented smart lazy-load on most crucial components</li>
+                            </ol>
+                     </div>
+                    <li>Ci/CD Pipeline migration from Octopus + Jenkins + Teamcity to Gitlab CI for simplification across 50+ projects.</li>
+                        <div class="tw-ml-12 tw-text-gray-300 tw-text-sm">
+                            <ol style="list-style-type: lower-alpha; padding-bottom: 0;">
+                                <li>Simplified CI/CD by making components, including: SemVer, Var Substitution, File Transform, Build / Test, Deployment to AWS Lambda</li>
+                            </ol>
+                        </div>
+                    <li>Setup multi-environment Containerized CI pipeline for easy testing and deployment across dotnet Framework / Core / NetCore2.0</li>
+                    <div class="tw-ml-12 tw-text-gray-300 tw-text-sm">
+                            <ol style="list-style-type: lower-alpha; padding-bottom: 0;">
+                                <li>Created Dockerfiles, multiple environments and the rules for deploying / spinning up the correct environment for QA / Test / Prod</li>
+                                <li>Implemented caching for branches to speed up subsequent builds, as well as artifact handling for monitoring later</li>
+                            </ol>
+                        </div>
+                    <li>Improved backend-performance of crucial REST API to handle more than 150.000 events a minute.</li>
+                    <div class="tw-ml-12 tw-text-gray-300 tw-text-sm">
+                            <ol style="list-style-type: lower-alpha; padding-bottom: 0;">
+                                <li>Used Profiler to identify bottlenecks in code and fix it.</li>
+                                <li>Used LinQPad to test and identify slow LinQ statements, then optimized them.</li>
+                                <li>Improved SQL statements through better indexing and querying, taking the average call below 4ms</li>
+                                <li>Implemented REDIS cache for most commonly used data with high immutability</li>
+                            </ol>
+                        </div>
+                    <li>Increased ad-based revenue 44% over 3 months, as well as improved Lighthouse performance for better ranking</li>
+                    <div class="tw-ml-12 tw-text-gray-300 tw-text-sm">
+                            <ol style="list-style-type: lower-alpha; padding-bottom: 0;">
+                                <li>Improved GPT implementation, bundle size and CLS through better implementation of modern web-standards</li>
+                            </ol>
+                        </div>
+                </ol>
+
+            
+        </div>
+
+        <!-- Profile Picture Section -->
+        <div class="tw-flex tw-justify-center tw-w-1/2 tw-mx-auto lg:tw-pt-24 lg:tw-p-4">
+            <q-img 
+                src="/src/assets/ProfilePicture.jpg"
+                spinner-color="white"
+                loading="lazy"
+                fit="contain"
+                width="500px"
+            ></q-img>
         </div>
     </div>
 </template>
-<script lang="ts" setup></script>
-<style scoped></style>
+<script lang="ts" setup>
+
+</script>
+
+<style scoped></style> 
